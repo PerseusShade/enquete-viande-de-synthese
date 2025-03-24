1f63fb42585a1c8fd321159b3ae948162e5ac4fa
CyberGhost ;) <107486742+CyberGhost-2612@users.noreply.github.com>
2025-03-24 23:06:00 +0100

    Update menu-loader.js

--- assets/js/menu-loader.js
+++ assets/js/menu-loader.js
@@ -1,13 +1,3 @@
-// Charge le menu et la barre de titre dynamiquement
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/enquete-viande-de-synthese/components/menu.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("menu-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erreur lors du chargement du menu :", error));
-});
-
 const basePath = "/projet"; // Remplacez 'projet' par le nom de votre dépôt
 document.querySelectorAll('link, script, img, a').forEach(element => {
     ['href', 'src'].forEach(attr => {
@@ -18,4 +8,14 @@ document.querySelectorAll('link, script, img, a').forEach(element => {
             }
         }
     });
+});
+
+// Charge le menu et la barre de titre dynamiquement
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/enquete-viande-de-synthese/components/menu.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("menu-container").innerHTML = data;
+        })
+        .catch(error => console.error("Erreur lors du chargement du menu :", error));
 });
