e3007d64e2288e8c9b052580647c56b3361fa878
CyberGhost ;) <107486742+CyberGhost-2612@users.noreply.github.com>
2025-03-24 23:26:40 +0100

    Create menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,17 @@
+// Récupérer le chemin actuel de l'URL
+const currentPage = window.location.pathname;
+
+// Trouver l'élément du menu correspondant à l'URL actuelle
+if (currentPage.includes("index.html")) {
+    document.getElementById("home").classList.add("active");
+} else if (currentPage.includes("acteurs.html")) {
+    document.getElementById("acteurs").classList.add("active");
+} else if (currentPage.includes("discours.html")) {
+    document.getElementById("discours").classList.add("active");
+} else if (currentPage.includes("prospectives.html")) {
+    document.getElementById("prospectives").classList.add("active");
+} else if (currentPage.includes("sources.html")) {
+    document.getElementById("sources").classList.add("active");
+} else if (currentPage.includes("apropos.html")) {
+    document.getElementById("apropos").classList.add("active");
+}
