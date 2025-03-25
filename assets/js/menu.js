bfdc9ac3fb8d98a2f19981772846475cc5e882c4
CyberGhost ;) <107486742+CyberGhost-2612@users.noreply.github.com>
2025-03-25 17:46:39 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,9 +2,7 @@
 const currentPage = window.location.pathname;
 
 // Trouver l'élément du menu correspondant à l'URL actuelle
-if (currentPage.includes("index.html")) {
-    document.getElementById("home").classList.add("active");
-} else if (currentPage.includes("acteurs.html")) {
+if (currentPage.includes("acteurs.html")) {
     document.getElementById("acteurs").classList.add("active");
 } else if (currentPage.includes("discours.html")) {
     document.getElementById("discours").classList.add("active");
@@ -14,4 +12,6 @@ if (currentPage.includes("index.html")) {
     document.getElementById("sources").classList.add("active");
 } else if (currentPage.includes("apropos.html")) {
     document.getElementById("apropos").classList.add("active");
+} else {
+    document.getElementById("home").classList.add("active");
 }
