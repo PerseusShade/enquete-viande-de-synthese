56d4c31a2dd223e20c5df993d0b2927cc215cee1
CyberGhost ;) <107486742+CyberGhost-2612@users.noreply.github.com>
2025-03-30 23:56:39 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,3 +38,64 @@ function checkTime() {
         player.pauseVideo();
     }
 }
+
+function preloadImages(imageArray) {
+    imageArray.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+}
+
+const imagesToPreload = [
+    "../img/logo.png",
+    "../img/logoDauph.png",
+
+    "../img/acteurs/image1.png",
+    "../img/acteurs/image2.png",
+    "../img/acteurs/image3.png",
+    "../img/acteurs/image4.png",
+    "../img/acteurs/image5.png",
+    "../img/acteurs/image6.png",
+    "../img/acteurs/image7.png",
+    "../img/acteurs/image8.png",
+    "../img/acteurs/image9.png",
+    "../img/acteurs/image10.png",
+    "../img/acteurs/image11.png",
+    "../img/acteurs/image12.png",
+    "../img/acteurs/image13.png",
+
+    "../img/discours/split-image.png",
+    "../img/discours/surquoi.png",
+
+    "../img/prospectives/adoption-progre.png",
+    "../img/prospectives/adoption_totale.png",
+    "../img/prospectives/projet_complet.png",
+
+    "../img/sources/image1.png",
+    "../img/sources/image2.png",
+    "../img/sources/image3.png",
+    "../img/sources/image4.png",
+    "../img/sources/image5.png",
+    "../img/sources/image6.png",
+    "../img/sources/image7.png",
+    "../img/sources/image8.png",
+    "../img/sources/image9.png",
+    "../img/sources/image10.png",
+    "../img/sources/image11.png",
+    "../img/sources/image12.png",
+    "../img/sources/image13.png",
+    "../img/sources/image14.png",
+    "../img/sources/image15.png",
+    "../img/sources/image16.png",
+    "../img/sources/image17.png",
+    "../img/sources/image18.png",
+    "../img/sources/image19.png",
+    "../img/sources/image20.png",
+    "../img/sources/image21.png",
+    "../img/sources/image22.png",
+    "../img/sources/image23.png"
+];
+
+window.addEventListener("load", function() {
+    preloadImages(imagesToPreload);
+});
